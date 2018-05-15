8e1fc4a9d04262b7f2e2fad678df45356a9e41c5
0652015 <ilove2426you@gmail.com>
2018-05-15 23:53:57 +0800

    加法

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("yoyo");
+	int addend=3 ,augend=2,sum=0;//被加數和加數和總和 
+	sum=augend+addend;//3+2
+	printf("sum=%d",sum);
 	return 0;
 }
